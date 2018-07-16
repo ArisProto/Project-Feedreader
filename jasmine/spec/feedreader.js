22dc6d50a59fb49e945a7eb52a0b07c9c4826b15
Aris-Protopapas <aris.protopapas@gmail.com>
2018-07-16 19:23:46 +0200

    Add missing parenthesis

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -63,8 +63,6 @@ $(function() {
 
     describe('The menu', function() {
 
-    });
-
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -83,7 +81,8 @@ $(function() {
 
           it('Menu toggleability', function() {
 
-          };
+          });
+      });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
