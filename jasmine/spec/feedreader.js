9887d335d5a34278b3aa238393ba2f34f9e55a6d
Aris-Protopapas <aris.protopapas@gmail.com>
2018-07-16 20:29:05 +0200

    Added missing parenthesis

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,18 +1,5 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
-  /* This is our first test suite - a test suite just contains
-  * a related set of tests. This suite is all about the RSS
-  * feeds definitions, the allFeeds variable in our application.
-  */
+
   describe('RSS Feeds', function() {
     it('are defined', function() {
       expect(allFeeds).toBeDefined();
@@ -108,6 +95,7 @@ $(function() {
          expect($('.entry h2').length).not.toBe([0]);
          done();
      });
+   });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
