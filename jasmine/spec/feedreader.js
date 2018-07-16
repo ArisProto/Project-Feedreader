fd3416ea865da1e5fa35184d522745f97c66cb88
Aris-Protopapas <aris.protopapas@gmail.com>
2018-07-16 19:05:21 +0200

    Add var UrlR for regular expressions

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -33,7 +33,10 @@ $(function() {
          */
 
         it('Url is defined and not empty', function() {
+          allFeeds.forEach(function (feed, i) {
+            var UrlR = (https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,});
 
+          });
         });
 
         /* TODO: Write a test that loops through each feed
